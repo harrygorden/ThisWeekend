48bcc580411723ff525a144988ea83b2c333d3c8
harrygorden <harrygorden@gmail.com>
2025-01-21 00:46:33 -0600

    Continued Troubleshooting

--- server_code/LangChainModules.py
+++ server_code/LangChainModules.py
@@ -4,6 +4,7 @@ from anvil.tables import app_tables
 import anvil.secrets
 import anvil.server
 from langchain_text_splitters import RecursiveJsonSplitter
+from . import CoreServerModule
 
 # This is a server module. It runs on the Anvil server,
 # rather than in the user's browser.
