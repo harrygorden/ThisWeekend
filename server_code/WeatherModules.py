19f24933f809e439bdb993dbe0d20018519b4e27
harrygorden@gmail.com <harrygorden@gmail.com>
2025-01-21 03:18:29 +0000

    Edited WeatherModules

--- server_code/WeatherModules.py
+++ server_code/WeatherModules.py
@@ -4,6 +4,7 @@ from anvil.tables import app_tables
 import anvil.secrets
 import anvil.server
 import requests
+from datetime import datetime
 
 #  This module runs on an Anvil server in the server environment.
 #  It is not run in the user's browser.  All functions defined in this
@@ -21,7 +22,7 @@ def get_weather_openweathermap():
     
     # Add new row to weatherdata table with current timestamp and weather data
     app_tables.weatherdata.add_row(
-        timestamp=tables.now(),
+        timestamp=datetime.now(),
         weatherdata_openweathermap=weather_data
     )
     
