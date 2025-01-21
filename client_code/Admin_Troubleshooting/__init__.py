876be319910fa38f8005a649723b2da9bbccf3b9
harrygorden@gmail.com <harrygorden@gmail.com>
2025-01-21 02:29:07 +0000

    Created Admin_Troubleshooting, CoreServerModule, EventModules, LICENSE.txt and 2 more; Edited settings and theme/parameters.yaml; Deleted Form1

--- client_code/Admin_Troubleshooting/__init__.py
+++ client_code/Admin_Troubleshooting/__init__.py
@@ -1,8 +1,9 @@
-from ._anvil_designer import Form1Template
+from ._anvil_designer import Admin_TroubleshootingTemplate
 from anvil import *
+import anvil.server
 
 
-class Form1(Form1Template):
+class Admin_Troubleshooting(Admin_TroubleshootingTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
